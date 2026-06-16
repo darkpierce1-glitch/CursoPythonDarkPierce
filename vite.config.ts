2cf5101dcb21748fa05ea92efb0f93d2e952bc5b
JonathanArroyaveGonzalez <arroyave3000g@gmail.com>
2026-06-15 21:20:59 -0500

    fix: fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@api": path.resolve(__dirname, "./api"),
     },
   },
   // Importante para React Router en Vercel: forzar rutas relativas.
