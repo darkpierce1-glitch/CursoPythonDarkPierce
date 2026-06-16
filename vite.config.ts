5b4ae2ff439a0c70baf87b7c34a70e0d0b08b874
JonathanArroyaveGonzalez <arroyave3000g@gmail.com>
2026-06-15 21:43:11 -0500

    fix deploy proxy

--- vite.config.ts
+++ vite.config.ts
@@ -20,15 +20,6 @@ export default defineConfig({
     },
   },
   base: './',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
